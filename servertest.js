b0ab8c4efb1f77e5914a05db16bd137f7f04cff7
pinsara.ecyber <pinsara@ecyber.com>
2023-08-15 00:27:49 +0530

    test script

--- servertest.js
+++ servertest.js
@@ -1,15 +1,13 @@
 
 "use strict";
-const electron = require("electron");
 const path = require("path");
-const utils = require("@electron-toolkit/utils");
 const icon = path.join(__dirname, "./resources/icon.png");
 const express = require('express');
 const { spawn } = require("child_process");
 const platform = require("os").platform()
 
 const app = express();
-const PORT = 5173;
+const PORT = 9090;
 
 
 app.get('/test', (req, res) => {
@@ -44,42 +42,3 @@ if (/^win/.test(platform)) {
 // });
 
 app.listen(PORT, () => console.log(`HTTP server started on port: ${PORT}`));
-
-function createWindow() {
-  const mainWindow = new electron.BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
-    autoHideMenuBar: true,
-    ...process.platform === "linux" ? { icon } : {},
-    webPreferences: {
-      preload: path.join(__dirname, "./out/preload/index.js"),
-      sandbox: false
-    }
-  });
-  mainWindow.on("ready-to-show", () => {
-    mainWindow.show();
-  });
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    electron.shell.openExternal(details.url);
-    return { action: "deny" };
-  });
-
-  mainWindow.loadURL(`http://localhost:${PORT}`);
-}
-
-electron.app.whenReady().then(() => {
-  utils.electronApp.setAppUserModelId("com.electron");
-  electron.app.on("browser-window-created", (_, window) => {
-    utils.optimizer.watchWindowShortcuts(window);
-  });
-  createWindow();
-  electron.app.on("activate", function () {
-    if (electron.BrowserWindow.getAllWindows().length === 0)
-      createWindow();
-  });
-});
-electron.app.on("window-all-closed", () => {
-  electron.app.quit();
-  // process.kill();
-});
