2692817c673e30c65666babce340e439ad80b311
pinsara.ecyber <pinsara@ecyber.com>
2023-08-15 00:41:18 +0530

    with process

--- program.js
+++ program.js
@@ -16,32 +16,30 @@ app.get('/test', (req, res) => {
   res.send('HTTP server working!');
 });
 
-// let process = null;
+let process = null;
 
 app.use(express.static(path.join(__dirname, './out/renderer')));
 
 if (/^win/.test(platform)) {
-  // app.use(express.static(path.join(__dirname,'.\\out\\renderer')));
-  // process = spawn('.\\node_modules\\.bin\\strapi', ['start'], { cwd: '.\\physicsclz-backend', shell: true })
+  process = spawn('.\\node_modules\\.bin\\strapi', ['start'], { cwd: '.\\physicsclz-backend', shell: true })
 } else {
-  // app.use(express.static());
-  // process = spawn('./node_modules/.bin/strapi', ['start'], { cwd: './physicsclz-backend', shell: true })
+  process = spawn('./node_modules/.bin/strapi', ['start'], { cwd: './physicsclz-backend', shell: true })
 }
 
-// process.stdout.on('data', (output) => {
-//   if(output.includes('Welcome back!')) {
+process.stdout.on('data', (output) => {
+  if(output.includes('Welcome back!')) {
 
-//   }
-//   console.log(`${output}`);
-// });
+  }
+  console.log(`${output}`);
+});
 
-// process.on('error', (error) => {
-//   console.error(`Error: ${error.message}`);
-// });
+process.on('error', (error) => {
+  console.error(`Error: ${error.message}`);
+});
 
-// process.on('close', (code) => {
-//   console.log(`Strapi process exited with code ${code}`);
-// });
+process.on('close', (code) => {
+  console.log(`Strapi process exited with code ${code}`);
+});
 
 app.listen(PORT, () => console.log(`HTTP server started on port: ${PORT}`));
 
@@ -81,5 +79,5 @@ electron.app.whenReady().then(() => {
 });
 electron.app.on("window-all-closed", () => {
   electron.app.quit();
-  // process.kill();
+  process.kill();
 });
